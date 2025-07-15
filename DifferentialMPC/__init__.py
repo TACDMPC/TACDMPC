0586c3f26bd72963bdfc4e098f65b6679543fede
AntoBuo-Dev <buoantonio97@gmail.com>
2025-07-15 10:12:37 +0200

    Add export list to DifferentialMPC package (#20)

--- DifferentialMPC/__init__.py
+++ DifferentialMPC/__init__.py
@@ -3,3 +3,12 @@ from .controller import DifferentiableMPCController
 from .controller import GradMethod
 from .utils import pnqp
 from .utils import batched_jacobian, jacobian_finite_diff_batched
+
+__all__ = [
+    "GeneralQuadCost",
+    "DifferentiableMPCController",
+    "GradMethod",
+    "pnqp",
+    "batched_jacobian",
+    "jacobian_finite_diff_batched",
+]
